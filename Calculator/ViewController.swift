87d4df428c4fdc0566c87688a000fc712bc341f1
Devin Smaldore <devinsmaldore@devins-macbook-pro.local>
2017-01-29 18:00:34 -0500

    stack fully implemented, working on connecting buttons using IBAction and doMath function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,35 +1,88 @@
-//
+//  Name: Devin Smaldore
+//  Date: 1-27-17
+//  Class: csc2310
+//  Comments: Calculator App
 //  ViewController.swift
 //  Calculator
-//
-//  Created by Jackson Inchalik on 1/26/17.
-//  Copyright © 2017 Jackson Inchalik. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    struct Stack{
-        var items = [Int]()
-        
-        mutating func push(_ newItem: Int){
-            items.append(newItem)
+//create stack of any element type
+struct Stack<Element> {
+    var items = [Element]()
+    
+    mutating func push(_ newItem: Element) {
+        items.append(newItem)
     }
-        
-    mutating func pop() -> Int? {
+    
+    mutating func pop() -> Element? {
         guard !items.isEmpty else {
             return nil
         }
         return items.removeLast()
     }
+    
+    func top() -> Element? {
+        guard !items.isEmpty else {
+            return nil
+        }
+        return items.last
+    }
+    
+    func isEmpty() -> Bool? {
+        return items.isEmpty
+    }
+}
+
+class ViewController: UIViewController {
+
+    @IBAction func addToNumStack(_ newOp: Int, sender: UIButton) {
+        var numStack = Stack<Int>()
+        
+        numStack.push(newOp)
+    }
+    
+    func addToOpStack(_ newOp: Character) {
+        var opStack = Stack<Character>()
+        
+        if opStack.isEmpty()! {
+            opStack.push(newOp)
+        } else if opStack.top() == "+" || opStack.top() == "-" {
+            opStack.push(newOp)
+        } else if opStack.top() == "*" || opStack.top() == "/" {
+            if newOp == "*" || newOp == "/" {
+                opStack.push(newOp)
+            } else {
+                
+            }
+        }
+        
+        //opStack.push(newOp)
+    }
+    
+    func doMath(_ operand1: Int, operand2: Int, operator1: Character) -> Int {
+        var result = 0
+        
+        if operator1 == "+" {
+            result = operand1 + operand2
+        } else if operator1 == "-" {
+            result = operand1 - operand2
+        } else if operator1 == "*" {
+            result = operand1 * operand2
+        } else if operator1 == "/" {
+            result = operand1 / operand2
+        }
+        return result
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func handleNumButtonPress(_ sender: UIButton) {
+        
+    }
+    
 }
-
